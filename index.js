3b5d91d1c2a976deb73e71366df0f6e8bf862e71
Evan Beck <evandrydenbeck@gmail.com>
2022-01-24 19:36:59 -0500

    commit tree def8509605576c0b7191a8d1e02a5e75f440e64d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+// Challenge
+function submitData (name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+        .then(response => response.json())
+        .then(userData => document.body.innerHTML = userData["id"])
+        .catch(error => document.body.innerHTML = error.message)
+}
